21bb8a6fa7a461d007c6b6e05e0995d699d47323
vizucode <havisikkubaru@gmail.com>
2022-07-28 10:29:58 +0700

    fix(redis-client): fix redis client environment

--- config/redis.go
+++ config/redis.go
@@ -1,16 +1,14 @@
 package config
 
 import (
+	"os"
+
 	"github.com/go-redis/redis/v9"
 )
 
-// var REDIS_ADDR = os.Getenv("REDIS_ADDR")
-// var REDIS_PASS = os.Getenv("REDIS_PASS")
-// var REDIS_USERNAME = os.Getenv("REDIS_USERNAME")
-
-var REDIS_ADDR = "localhost:6379"
-var REDIS_PASS = "secret"
-var REDIS_USERNAME = "havis"
+var REDIS_ADDR = os.Getenv("REDIS_ADDR")
+var REDIS_PASS = os.Getenv("REDIS_PASS")
+var REDIS_USERNAME = os.Getenv("REDIS_USERNAME")
 var REDIS_DB = 0
 
 func NewRedisClient() *redis.Client {
